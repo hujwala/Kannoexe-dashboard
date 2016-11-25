70a2fd01fba2ad10c0814a5c83c6e58ee0ae1106
hujwala <upatel@qwinixtech.com>
2016-11-25 17:41:57 +0530

    sprint planning

--- jobs/sprint_progress.rb
+++ jobs/sprint_progress.rb
@@ -1,11 +1,10 @@
 require 'jira-ruby'
 
-
-SCHEDULER.every '1h', :first_in => 0 do |job|
+SCHEDULER.every '2m', :first_in => 0 do |job|
   client = JIRA::Client.new({
-    :username =>"upatel" ,
+    :username => "upatel",
     :password => "Qwinix123",
-    :site => "https://qwinix.atlassian.net",
+    :site => "https://qwinix.atlassian.net/secure/RapidBoard.jspa?rapidView=288",
     :auth_type => :basic,
     :context_path => ""
   })
